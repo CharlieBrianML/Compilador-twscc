5f3a6c383562d81adecd6cc4ac5c5fec3022782a
Erik Valdez Mondragon <perik_92@hotmail.com>
2019-03-10 00:55:32 -0600

    commit desde atom

---
+++ lib/proyecto_compilador.ex
@@ -0,0 +1,18 @@
+defmodule ProyectoCompilador do
+  @moduledoc """
+  Documentation for ProyectoCompilador.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProyectoCompilador.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
