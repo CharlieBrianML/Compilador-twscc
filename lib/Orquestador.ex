d0ba60418a7b0c3d4bce4cd85857f838711c4900
Erik Valdez Mondragon <perik_92@hotmail.com>
2019-04-02 11:18:01 -0600

    Se sustituyen if-else por case para mostrar estructuras segun banderas

--- lib/Orquestador.ex
+++ lib/Orquestador.ex
@@ -2,23 +2,20 @@ defmodule Orquestador do
 
   def manager(arg, file_adress) do
 
-    file_content=  File.read!(file_adress); ##lectura del archivo
+    file_content= File.read!(file_adress); ##lectura del archivo
+    tokens_separados =  Lexer.scan_word(file_content); #generación de Tokens
+    ast = Parser.parsero(tokens_separados); #generación del árbol
+    ensamblador=Generador_codigo.assembly(ast); #generación del código
 
-    tokens_separados =  Lexer.scan_word(file_content);
-    if arg == "--tokens" or "--n" do
-          IO.inspect(tokens_separados);
-    else
+    case arg do
+      "-t" -> IO.inspect(tokens_separados);
+      "-a" -> IO.inspect(ast);
+      "-s" -> IO.inspect(ensamblador)
+      "--debug" -> IO.inspect(tokens_separados)
+                   IO.inspect(ast);
+                   IO.puts(ensamblador)
+      nil -> :ok
     end
 
-    ast = Parser.parsero(tokens_separados);
-    if arg == "--ast" or "-a" do
-          IO.inspect(ast);
-        else
-        end
-    ensamblador=Generador_codigo.assembly(ast);
-    if arg == "--S" do
-    else end
-
   end
-
 end
